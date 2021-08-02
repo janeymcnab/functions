4f54355dfb1030afcb6d3c3a2407e5f9eb15b3e6
Jane McNab <writetojaney@hotmail.co.uk>
2021-08-02 12:53:43 +0100

    initial commit

---
+++ src/python_functions_practice.py
@@ -0,0 +1,48 @@
+def return_10(): 
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+     return string_1 + string_2 
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(month):
+    if month == 1:
+        return "January"
+    if month == 3:
+        return "March"
+    if month == 9:
+        return "September"
+
+
+def number_to_short_month_name(month):
+    if month == 1:
+        return "Jan"
+    if month == 4:
+        return "Apr"
+    if month == 10:
+        return "Oct"
+
+
+def reverse_string():
+    
+
+    
+
